b52e16af17c55192db3a13650ec2d3696cdaae37
samoht <53660242+totobighouse@users.noreply.github.com>
2020-11-18 02:20:56 -0300

    made pip package and moved all code to __init__.py

---
+++ clickTwitch/__init__.py
@@ -0,0 +1,37 @@
+import mouse
+
+def detectPosition():
+    '''
+    Waits until client presses middle button, then parses the mouse position to variables used by clickPosition().
+    '''
+    print('Position your mouse where the claiming button is. When you are ready, click the middle button on your mouse to confirm the location')
+    mouse.wait(button='middle')
+    pos = mouse.get_position()
+
+    detectPosition.x = pos[0]
+    detectPosition.y = pos[1]
+
+def clickPosition(x, y):
+    '''
+    Moves to passed coordenate and clicks.
+    '''
+    mouse.move(x, y, absolute=True, duration=0.1) 
+    mouse.click(button='left')
+
+def randomMovement():
+    '''
+    Moves to random position, to prevent twitch from detecting the clicker.
+    '''
+    import random
+
+    xRandomPosition = random.randrange(-50, 50)
+    yRandomPosition = random.randrange(-50, 50)
+    mouse.move(xRandomPosition, yRandomPosition, absolute=False, duration=0.1)
+
+def isLetterY(letter):
+    '''
+    Returns true is wantsRandom is equal to 'y' (yes).
+    '''
+    if letter == 'y' or letter == 'Y':
+        return True
+    return False
