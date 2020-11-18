26cdd7c9a5d255f84a59fa0e38ea15f6c4bb2e64
samoht <53660242+totobighouse@users.noreply.github.com>
2020-11-18 02:23:01 -0300

    added setup.py for pip packaging

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="clickTwitch",
+    version="1.2.0",
+    author="samoht9277",
+    author_email="totobighouse@gmail.com",
+    description="clickTwitch is an automation tool that claims your twitch reward while watching a stream",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/samoht9277/clickTwitch",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'mouse',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.0'
+)
